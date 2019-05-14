1b935d978b51c064caaa00ef50c16184d13359b2
Pablo Piskunow <pablo.perez.piskunow@gmail.com>
2019-05-14 16:33:27 +0200

    format in tests

--- codes/tests/test_higher_order.py
+++ codes/tests/test_higher_order.py
@@ -1,9 +1,12 @@
 import numpy as np
 import sympy
-from codes.higher_order_lowdin import PerturbativeModel, get_effective_model, allclose
-
+from codes.higher_order_lowdin import get_effective_model
+from codes.perturbative_model import PerturbativeModel
+from ..qsymm.linalg import allclose
 
 def test_simple_model():
+    order = 8
+    rtol = 2e-2
     # Simple model
     ev2 = np.array([0., 1.])
     mat02 = np.diag(ev2)
@@ -20,7 +23,7 @@ def test_simple_model():
     evec_A = evec2[:, indices2]
     evec_B = evec2[:, np.array([1])]
 
-    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=8)
+    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=order)
 
     # Should match within default tolerance
     assert model == exact_result, model - exact_result
@@ -30,7 +33,10 @@ def test_simple_model():
     # Simple model
     ev2 = np.array([0, 0, 1, 1])
     mat02 = np.diag(ev2)
-    mat12 = {sympy.Symbol('x'): np.array([[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 2, 0], [1, 0, 0, 2]])}
+    mat12 = {sympy.Symbol('x'): np.array([[0, 0, 0, 1],
+                                          [0, 0, 1, 0],
+                                          [0, 1, 2, 0],
+                                          [1, 0, 0, 2]])}
     evec2 = np.eye(4)
     indices2 = [0, 1]
 
@@ -41,41 +47,22 @@ def test_simple_model():
     evec_A = evec2[:, indices2]
     evec_B = evec2[:, np.array([2, 3])]
 
-    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=8)
+    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=order)
 
     # Should match within default tolerance
     assert model == exact_result, model - exact_result
 
     # Test KPM method
-    kpm_params = dict(num_moments=1000)
-    evec_A = evec2[:, indices2]
-    evec_B = None
+    for kpm_params, precalc in [(dict(num_moments=1000), False),
+                                (dict(num_moments=1000), True),
+                                (dict(energy_resolution=0.01), False)]:
+        evec_A = evec2[:, indices2]
+        evec_B = None
 
-    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=8, kpm_params=kpm_params)
+        model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B,
+                                    order=order, kpm_params=kpm_params,
+                                    _precalculate_moments=precalc)
 
-    # Should match wit larger tolerance
-    for key in model.keys():
-        assert allclose(model[key], exact_result[key], rtol=2e-2), (key, model[key] - exact_result[key])
-
-    # Test KPM method with precalculated moments
-    kpm_params = dict(num_moments=1000)
-    evec_A = evec2[:, indices2]
-    evec_B = None
-
-    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=8,
-                                kpm_params=kpm_params, _precalculate_moments=True)
-
-    # Should match wit larger tolerance
-    for key in model.keys():
-        assert allclose(model[key], exact_result[key], rtol=2e-2), (key, model[key] - exact_result[key])
-
-    # Test KPM method with energy resolution
-    kpm_params = dict(energy_resolution=0.01)
-    evec_A = evec2[:, indices2]
-    evec_B = None
-
-    model = get_effective_model(mat02, mat12, evec_A, evec_B=evec_B, order=8, kpm_params=kpm_params)
-
-    # Should match wit larger tolerance
-    for key in model.keys():
-        assert allclose(model[key], exact_result[key], rtol=2e-2), (key, model[key] - exact_result[key])
+        # Should match wit larger tolerance
+        for key in model.keys():
+            assert allclose(model[key], exact_result[key], rtol=rtol), (key, model[key] - exact_result[key])
