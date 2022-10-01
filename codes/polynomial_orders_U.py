646342eaf742e3fa4d236a04ff0665ca7ba89ec1
Anton Akhmerov <anton.akhmerov@gmail.com>
2022-10-01 22:45:19 +0200

    more debugging, complete perturbation testing (still bugs)

--- codes/polynomial_orders_U.py
+++ codes/polynomial_orders_U.py
@@ -68,8 +68,9 @@ def H_tilde(H_0, H_p, wanted_order, U_n, V_n):
         first_term = zero
         second_term = zero
     else:
-        first_term = V_n[0]
-        second_term = V_n[0]
+        first_term = np.zeros_like(V_n[0])
+        second_term = np.zeros_like(V_n[0])
+
     for n in range(0, wanted_order+1):
         for i in range(0, n + 1):
             first_term += (U_n[n-i] - V_n[n-i]) @ H_0 @ (U_n[i] + V_n[i])
@@ -85,19 +86,17 @@ sympy.block_collapse(H_tilde_n[1]).blocks[0, 1] # should be 0 and give condition
 
 sympy.block_collapse(H_tilde_n[1]).blocks[1, 0] # should be 0 and give condition for V^AB
 
+
 # ### Computing $U_n$ and $V_n$
 
 # These are the blocks computed recursively
 # $$
 # \begin{align}
-# -2U_n^{AA} &= \sum_{i=1}^{n-i} \left(U_{n-i}^{AA}U_i^{AA} + U_{n-i}^{AB}(U_i^{AB})^\dagger + U_{n-i}^{AB}(V_i^{AB})^\dagger + V_{n-i}^{AB}(U_i^{AB})^\dagger + V_{n-i}^{AB}(V_i^{AB})^\dagger\right)\\
-# -2U_n^{BB} &= \sum_{i=1}^{n-i} \left(U_{n-i}^{BB}U_i^{BB} + (U_{n-i}^{AB})^\dagger U_i^{AB} - (U_{n-i}^{AB})^\dagger V_i^{AB} - (V_{n-i}^{AB})^\dagger U_i^{AB} + (V_{n-i}^{AB})^\dagger V_i^{AB}\right)\\
-# -2U_n^{AB} &= \sum_{i=1}^{n-i} \left(U_{n-i}^{AA}U_i^{AB} + U_{n-i}^{AB}U_i^{BB} - V_{n-i}^{AB} U_i^{BB} + U_{n-i}^{AA} V_i^{AB}\right)\\
-# \end{align}.
+# -2U_n^{AA} &= \sum_{i=1}^{n-1} \left(U_{n-i}^{AA}U_i^{AA} + (U_{n-i}^{AB} + V_{n-i}^{AB})(U_i^{AB} + V_i^{AB})^\dagger\right)\\
+# -2U_n^{BB} &= \sum_{i=1}^{n-1} \left(U_{n-i}^{BB}U_i^{BB} + (U_{n-i}^{AB} - V_{n-i}^{AB})^\dagger (U_i^{AB} - V_{n-i}^{AB})\right)\\
+# -2U_n^{AB} &= \sum_{i=1}^{n-1} \left(U_{n-i}^{AA}(U_i^{AB} + V_i^{AB}) + (U_{n-i}^{AB} - V_{n-i}^{AB}) U_i^{BB}\right)\\
+# \end{align}
 # $$
-
-np.arange(4).reshape(-1, 1) + np.arange(5)
-
 
 def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
     """
@@ -115,6 +114,8 @@ def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
     if N_A is None:
         N_A = N // 2
     N_B = N - N_A
+    H_0_AA = H_0[:N_A, :N_A]
+    H_0_BB = H_0[N_A:, N_A:]
     H_p_AA = H_p[:N_A, :N_A]
     H_p_AB = H_p[:N_A, N_A:]
     H_p_BB = H_p[N_A:, N_A:]
@@ -125,7 +126,7 @@ def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
     U_ABn = [np.zeros((N_A, N_B), dtype=complex)]
     U_BBn = [np.eye(N_B, dtype=complex)]
     V_ABn = [np.zeros((N_A, N_B), dtype=complex)]
-    if wanted_order = 0:
+    if wanted_order == 0:
         return U_AAn, U_ABn, U_BBn, V_ABn
     
     #1st order
@@ -136,55 +137,49 @@ def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
     U_AAn.append(np.zeros((N_A, N_A), dtype=complex))
     U_ABn.append(np.zeros((N_A, N_B), dtype=complex))
     U_BBn.append(np.zeros((N_B, N_B), dtype=complex))
-    V_ABn.append((       
-                 U_AAn[0] @ H_p_AA @ (U_ABn[0] + V_ABn[0]) +
-                 U_AAn[0] @ H_p_AB @ U_BBn[0] +
-                 (U_ABn[0] - V_ABn[0]) @ H_p_AB.conj().T @ (U_ABn[0] + V_ABn[0]) +
-                 (U_ABn[0] - V_ABn[0]) @ H_p_BB @ U_BBn[0]
-                 ) * energy_denominators
-                ) # V_1
-    if wanted_order = 1:
+    V_ABn.append(-H_p_AB * energy_denominators)
+    if wanted_order == 1:
         return U_AAn, U_ABn, U_BBn, V_ABn
 
     for n in range(2, wanted_order+1):
         U_AA_next = np.zeros((N_A, N_A), dtype=complex)
         U_AB_next = np.zeros((N_A, N_B), dtype=complex)
         U_BB_next = np.zeros((N_B, N_B), dtype=complex)
+        # 0th term from the sum in the expression from the note
         Y_next = -(
-            U_AAn[n-1] @ H_p_AA @ (U_ABn[0] + V_ABn[0]) +
-            U_AAn[n-1] @ H_p[:N_A, N_A:] @ U_BBn[0] +
-            (U_ABn[n-1] - V_ABn[n-1]) @ H_p[:N_A, N_A:].conj().T @ (U_ABn[0] + V_ABn[0]) +
-            (U_ABn[n-1] - V_ABn[n-1]) @ H_p[N_A:, N_A:] @ U_BBn[0]
+            U_AAn[n-1] @ H_p_AA @ (U_ABn[0] + V_ABn[0])
+            + U_AAn[n-1] @ H_p_AB @ U_BBn[0]
+            + (U_ABn[n-1] - V_ABn[n-1]) @ H_p_AB.conj().T @ (U_ABn[0] + V_ABn[0])
+            + (U_ABn[n-1] - V_ABn[n-1]) @ H_p_BB @ U_BBn[0]
         )
         for i in range(1, n):
             U_AA_next -= (
-                U_AAn[n-i] @ U_AAn[i] + U_ABn[n-i] @ U_ABn[i].conj().T +
-                U_ABn[n-i] @ V_ABn[i].conj().T + V_ABn[n-i] @ U_ABn[i].conj().T +
-                V_ABn[n-i] @ V_ABn[i].conj().T
+                U_AAn[n-i] @ U_AAn[i]
+                + (U_ABn[n-i] + V_ABn[n-i]) @ (U_ABn[i] + V_ABn[i]).conj().T
             ) / 2
             U_AB_next -= (
-                U_AAn[n-i] @ U_ABn[i] + U_ABn[n-i] @ U_BBn[i] -
-                V_ABn[n-i] @ U_BBn[i] + U_AAn[n-i] @ V_ABn[i]
+                U_AAn[n-i] @ (U_ABn[i] + V_ABn[i]) + (U_ABn[n-i] - V_ABn[n-i]) @ U_BBn[i]
             ) / 2
             U_BB_next -= (
-                U_BBn[n-i] @ U_BBn[i] + U_ABn[n-i].conj().T @ U_ABn[i] -
-                U_ABn[n-i].conj().T @ V_ABn[i] - V_ABn[n-i].conj().T @ U_ABn[i] +
-                V_ABn[n-i].conj().T @ V_ABn[i]
+                U_BBn[n-i] @ U_BBn[i]
+                + (U_ABn[n-i] - V_ABn[n-i]).conj().T @ (U_ABn[i] - V_ABn[i])
             ) / 2
             Y_next -= (
-                U_ABn[n-i] @ H_p_AB.T.conj() @ (U_ABn[i] + V_ABn[i]) +
-                (U_ABn[n-i] - V_ABn[n-i]) @ H_p_BB @ U_BBn[i] +
-                U_AAn[n-i-1] @ H_p_AA @ (U_ABn[i] + V_ABn[i]) +
-                U_AAn[n-i-1] @ H_p_AB @ U_BBn[i] +
-                (U_ABn[n-i-1] - V_ABn[n-i-1]) @ H_p_AB.conj().T @ (U_ABn[i] + V_ABn[i]) +
-                (U_ABn[n-i-1] - V_ABn[n-i-1]) @ H_p_BB @ U_BBn[i]
+                # H_0 terms
+                U_AAn[n-i] @ H_0_AA @ (U_ABn[i] + V_ABn[i])
+                - (U_ABn[n-i] - V_ABn[n-i]) @ H_0_BB @ U_BBn[i]
+                # H_1 terms
+                + U_AAn[n-i-1] @ H_p_AA @ (U_ABn[i] + V_ABn[i])
+                + U_AAn[n-i-1] @ H_p_AB @ U_BBn[i]
+                + (U_ABn[n-i-1] - V_ABn[n-i-1]) @ H_p_AB.conj().T @ (U_ABn[i] + V_ABn[i])
+                + (U_ABn[n-i-1] - V_ABn[n-i-1]) @ H_p_BB @ U_BBn[i]
             )
 
         U_AAn.append(U_AA_next)
         U_ABn.append(U_AB_next)
         U_BBn.append(U_BB_next)
 
-        Y_next -= H_0[:N_A, :N_A] @ U_AB_next + U_AB_next @ H_0[N_A:, N_A:]
+        Y_next -= H_0_AA @ U_AB_next + U_AB_next @ H_0_BB
         V_ABn.append(Y_next * energy_denominators)
         
     return U_AAn, U_ABn, U_BBn, V_ABn
@@ -193,27 +188,57 @@ def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
 # ### Testing
 
 # +
-wanted_order = 4
+wanted_order = 2
 N_A = 4
 N_B = 5
 N = N_A + N_B
 H_0 = np.diag(np.sort(np.random.randn(N)))
 
-strength = 0.0001
 H_p = np.random.random(size=(N, N)) + 1j * np.random.random(size=(N, N))
 H_p += H_p.conj().T
-H_p = strength * H_p
+H_p = H_p
 # -
 
 U_AAn, U_ABn, U_BBn, V_ABn = compute_next_orders(H_0, H_p, wanted_order, N_A=N_A)
-
-evals, evecs = np.linalg.eigh(H_0 + H_p)
+assert all(U_AA.shape == (N_A, N_A) for U_AA in U_AAn)
+assert all(U_AB.shape == (N_A, N_B) for U_AB in U_ABn)
+assert all(U_BB.shape == (N_B, N_B) for U_BB in U_BBn)
+assert all(V_AB.shape == (N_A, N_B) for V_AB in V_ABn)
 
 # +
 U_n = [np.block([[U_AA, U_AB], [U_AB.conj().T, U_BB]]) for U_AA, U_AB, U_BB in zip(U_AAn, U_ABn, U_BBn)]
 V_n = [np.block([[np.zeros((N_A, N_A)), V_AB], [-V_AB.conj().T, np.zeros((N_B, N_B))]]) for V_AB in V_ABn]
 
 H_tilde_n = H_tilde(H_0, H_p, wanted_order, U_n, V_n)
+for H_tilde_ord in H_tilde_n:
+    non_hermiticity = np.linalg.norm(H_tilde_ord - H_tilde_ord.T.conj())
+    assert non_hermiticity < 1e-10, non_hermiticity
+
+def H_pert(strength, H_tilde_n):
+    return sum(
+        (strength**i * H for i, H in enumerate(H_tilde_n)),
+        np.zeros_like(H_tilde_n[0])
+    )
+
+def E_pert(strength, H_tilde_n):
+    return np.linalg.eigvalsh(H_pert(strength, H_tilde_n))
+
+def E_exact(strength, H_0, H_p):
+    return np.linalg.eigvalsh(H_0 + strength * H_p)
+
+strengths = np.logspace(-3, -1)
+pert_energies = np.array([E_pert(strength, H_tilde_n) for strength in strengths])
+exact_energies = np.array([E_exact(strength, H_0, H_p) for strength in strengths])
+plt.plot(strengths, np.abs(pert_energies - exact_energies) / strengths.reshape(-1, 1)**wanted_order)
+plt.loglog()
+plt.title("Energies should be exact up to wanted order")
+plt.figure()
+plt.plot(
+    strengths,
+    [np.linalg.norm(H_pert(strength, H_tilde_n)[:N_A, N_A:]) for strength in strengths] / strengths**wanted_order
+)
+plt.loglog()
+plt.title("Offdiagonal blocks should vanish to given order")
 # -
 
 # Exact diagonalization
@@ -249,5 +274,3 @@ plt.imshow(H_tilde_order,
            vmax=np.max(np.abs(H_tilde_order)),
            cmap='seismic')
 plt.colorbar()
-
-
