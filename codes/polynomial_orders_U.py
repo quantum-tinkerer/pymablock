fix some typos and errors, factor of 2 still somewhere

codes/polynomial_orders_U.py
@@ -7,6 +7,7 @@ from sympy import (
 )
 from sympy.physics.quantum.dagger import Dagger
 from sympy.physics.quantum import TensorProduct
+import matplotlib.pyplot as plt
 
 # ### Obtaining $\tilde{H}^{(n)AB}$
 
@@ -20,23 +21,29 @@ U_BB = Symbol('U^{BB}')
 V_AB = Symbol('V^{AB}')
 
 # +
-wanted_order = 3
+wanted_order = 4
 
-H_AA = MatrixSymbol('H^{AB}_0', N/2, N/2)
-H_BB = MatrixSymbol('H^{BA}_0', N/2, N/2)
+H_AA = MatrixSymbol('H^{AA}_0', N/2, N/2)
+H_BB = MatrixSymbol('H^{BB}_0', N/2, N/2)
 
 H_1_AA = MatrixSymbol('H^{AA}_1', N/2, N/2)
 H_1_BB = MatrixSymbol('H^{BB}_1', N/2, N/2)
 H_2_AB = MatrixSymbol('H^{AB}_2', N/2, N/2)
-H_2_BA = MatrixSymbol('H^{BA}_2', N/2, N/2)
+H_2_BA = Dagger(H_2_AB)
 
-U_AAn = [MatrixSymbol(U_AA.name + '_{}'.format(n), N/2, N/2) for n in range(0, wanted_order + 1)]
-U_ABn = [MatrixSymbol(U_AB.name + '_{}'.format(n), N/2, N/2) for n in range(0, wanted_order + 1)]
-U_BAn = [Dagger(U_AAn[n]) for n in range(0, wanted_order + 1)]
-U_BBn = [MatrixSymbol(U_BB.name + '_{}'.format(n), N/2, N/2) for n in range(0, wanted_order + 1)]
+U_AAn = [Identity(N/2), ZeroMatrix(N/2, N/2)]
+U_ABn = [ZeroMatrix(N/2, N/2), ZeroMatrix(N/2, N/2)]
+U_BAn = [ZeroMatrix(N/2, N/2), ZeroMatrix(N/2, N/2)]
+U_BBn = [Identity(N/2), ZeroMatrix(N/2, N/2)]
+U_AAn += [MatrixSymbol(U_AA.name + '_{}'.format(n), N/2, N/2) for n in range(2, wanted_order + 1)]
+U_ABn += [MatrixSymbol(U_AB.name + '_{}'.format(n), N/2, N/2) for n in range(2, wanted_order + 1)]
+U_BAn += [Dagger(U_ABn[n]) for n in range(2, wanted_order + 1)]
+U_BBn += [MatrixSymbol(U_BB.name + '_{}'.format(n), N/2, N/2) for n in range(2, wanted_order + 1)]
 
-V_ABn = [MatrixSymbol(V_AB.name + '_{}'.format(n), N/2, N/2) for n in range(0, wanted_order + 1)]
-V_BAn = [-Dagger(V_ABn[n]) for n in range(0, wanted_order + 1)]
+V_ABn = [ZeroMatrix(N/2, N/2), Identity(N/2)]
+V_BAn = [ZeroMatrix(N/2, N/2), -Identity(N/2)]
+V_ABn += [MatrixSymbol(V_AB.name + '_{}'.format(n), N/2, N/2) for n in range(2, wanted_order + 1)]
+V_BAn += [-Dagger(V_ABn[n]) for n in range(2, wanted_order + 1)]
 
 H_0 = BlockMatrix([[H_AA, ZeroMatrix(N/2, N/2)], [ZeroMatrix(N/2, N/2), H_BB]])
 H_p = BlockMatrix([[H_1_AA, H_2_AB], [H_2_BA, H_1_BB]])
@@ -50,29 +57,33 @@ zero = BlockMatrix([[ZeroMatrix(N/2, N/2), ZeroMatrix(N/2, N/2)], [ZeroMatrix(N/
 # -
 
 # $$
-# \tilde{H}^{(n)} = [H_0, V_n] + \{H_0, U_n'\} + \sum_{i=1}^{n-i} (U_{n-i}' - V_{n-i}) H_0 (U_i' + V_i) + \sum_{i=0}^{n-i} (U_{n-i-1}' - V_{n-i-1}) (H_1 + H_2) (U_i' + V_i).
+# \tilde{H}^{(n)} = \sum_{i=0}^n (U_{n-i} - V_{n-i}) H_0 (U_i + V_i) + \sum_{i=0}^{n-1} (U_{n-i-1} - V_{n-i-1}) H_p (U_i + V_i).
 # $$
 
 def H_tilde(H_0, H_p, wanted_order, U_n, V_n):
     """Returns H tilde to a certain order"""
     H_tilde_n = []
-    for n in range(wanted_order + 1):
-        first_term = H_0 @ V_n[n] - V_n[n] @ H_0
-        second_term = H_0 @ U_n[n] + U_n[n] @ H_0
-        third_term = U_n[1]
-        fourth_term = U_n[1]
-        for i in range(0, n+1):
-            if i == 0:
-                fourth_term += (U_n[n-i-1] - V_n[n-i-1]) @ H_p @ (U_n[i] + V_n[i])
-            else:
-                third_term += (U_n[n-i] - V_n[n-i]) @ H_0 @ (U_n[n] + V_n[n])
-                fourth_term += (U_n[n-i-1] - V_n[n-i-1]) @ H_p @ (U_n[i] + V_n[i])
-        H_tilde_n.append(first_term + second_term + third_term + fourth_term)
+    
+    if isinstance(V_n[0], sympy.matrices.expressions.blockmatrix.BlockMatrix):
+        first_term = zero
+        second_term = zero
+    else:
+        first_term = V_n[0]
+        second_term = V_n[0]
+    for n in range(0, wanted_order+1):
+        for i in range(0, n + 1):
+            first_term += (U_n[n-i] - V_n[n-i]) @ H_0 @ (U_n[i] + V_n[i])
+            if i<n:
+                second_term += (U_n[n-i-1] - V_n[n-i-1]) @ H_p @ (U_n[i] + V_n[i])
+        H_tilde_n.append(first_term + second_term)
     return H_tilde_n
 
 
 H_tilde_n = H_tilde(H_0, H_p, wanted_order, U_n, V_n)
+
 sympy.block_collapse(H_tilde_n[1]).blocks[0, 1] # should be 0 and give condition for V^AB
+
+sympy.block_collapse(H_tilde_n[1]).blocks[1, 0] # should be 0 and give condition for V^AB
 
 
 # ### Computing $U_n$ and $V_n$
@@ -104,30 +115,30 @@ def compute_next_orders(H_0, H_p, wanted_order):
     assert N%2==0 
     
     # Blocks of U and V
+    # 0th order
     U_AAn = [np.eye(N_A)]
     U_ABn = [np.zeros((N_A, N_B), dtype=complex)]
     U_BBn = [np.eye(N_B, dtype=complex)]
     V_ABn = [np.zeros((N_A, N_B), dtype=complex)]
     
-    if wanted_order == 0 :
-        return U_AAn, U_ABn, U_BBn, V_ABn
+    #1st order
+    E_A = np.diag(H_0)[:N_A]
+    E_B = np.diag(H_0)[N_A:]
+    energy_differences = 1/(np.tile(E_A, [N_B, 1]).T - np.tile(E_B, [N_A, 1]))
     
-    U_AAn.append(np.zeros((N_A, N_A), dtype=complex)) # U_1
+    U_AAn.append(np.zeros((N_A, N_A), dtype=complex))
     U_ABn.append(np.zeros((N_A, N_B), dtype=complex))
     U_BBn.append(np.zeros((N_B, N_B), dtype=complex))
-    V_ABn.append(       
+    V_ABn.append((       
                  U_AAn[0] @ H_p[:N_A, :N_A] @ (U_ABn[0] + V_ABn[0]) +
                  U_AAn[0] @ H_p[:N_A, N_A:] @ U_BBn[0] +
                  (U_ABn[0] - V_ABn[0]) @ H_p[:N_A, N_A:].conj().T @ (U_ABn[0] + V_ABn[0]) +
                  (U_ABn[0] - V_ABn[0]) @ H_p[N_A:, N_A:] @ U_BBn[0]
+                 )/energy_differences
                 ) # V_1
-    
-    if wanted_order == 1:
+    if wanted_order < 2:
         return U_AAn, U_ABn, U_BBn, V_ABn
     else:
-        E_A = np.diag(H_0)[:N_A]
-        E_B = np.diag(H_0)[N_A:]
-        energy_differences = 1/(np.tile(E_A, [N_B, 1]).T - np.tile(E_B, [N_A, 1]))
         for n in range(2, wanted_order+1):
             U_AA_next = np.zeros((N_A, N_A), dtype=complex)
             U_AB_next = np.zeros((N_A, N_B), dtype=complex)
@@ -175,11 +186,12 @@ def compute_next_orders(H_0, H_p, wanted_order):
 # ### Testing
 
 # +
-N_A = N_B = 2
-H_0 = np.diag([-1, -1, 1, 1])
+wanted_order = 4
+N_A = N_B = 4
+H_0 = np.diag([-1, -1, -1, -1, 1, 1, 1, 1])
 
-strength = 0.1
-H_p = np.random.random(size=(N_A+N_B, N_A+N_B)) + 1j * np.random.random(size=(4,4)) 
+strength = 0.0001
+H_p = np.random.random(size=(N_A+N_B, N_A+N_B)) + 1j * np.random.random(size=(N_A+N_B, N_A+N_B)) 
 H_p += H_p.conj().T
 H_p = strength * H_p
 # -
@@ -195,18 +207,38 @@ V_n = [np.block([[np.zeros((N_A, N_B)), V_AB], [-V_AB.conj().T, np.zeros((N_B, N
 H_tilde_n = H_tilde(H_0, H_p, wanted_order, U_n, V_n)
 # -
 
-import matplotlib.pyplot as plt
-plt.imshow(np.diag(evals.real))
+# Exact diagonalization
+plt.imshow(np.diag(evals.real), cmap='seismic')
 plt.colorbar()
 
-H_tilde_sum = np.sum([strength**i*H_tilde_n[i] for i in range(wanted_order)], axis=0)
+if wanted_order==0:
+    H_tilde_sum = H_tilde_n[0]
+else:
+    H_tilde_sum = np.sum(H_tilde_n, axis=0)
 
-plt.imshow(H_tilde_sum.real)
+# Lowdin result
+plt.imshow(H_tilde_sum.real, cmap='seismic')
 plt.colorbar()
 
-plt.imshow(np.diag(evals.real)-H_tilde_sum.real/2) # factor of 2 mismatch
+# Difference between Lowdin and exact diagonalization
+plt.imshow(np.diag(evals.real)-H_tilde_sum.real/(2*wanted_order+2),
+           vmin=-np.max(np.abs(np.diag(evals.real)-H_tilde_sum.real/(2*wanted_order+2))),
+           vmax=np.max(np.abs(np.diag(evals.real)-H_tilde_sum.real/(2*wanted_order+2))),
+           cmap='seismic'
+          ) # factor of 2 mismatch
 plt.colorbar()
 
 # :) :tada:
 
-# **There is a factor of 2 mismatch**, but that's for later 
+# **There is a factor of 2 mismatch per order of H**, but that's for later 
+
+# #### Other plots
+
+H_tilde_order = H_tilde_n[1].real
+plt.imshow(H_tilde_order,
+           vmin=-np.max(np.abs(H_tilde_order)),
+           vmax=np.max(np.abs(H_tilde_order)),
+           cmap='seismic')
+plt.colorbar()
+
+