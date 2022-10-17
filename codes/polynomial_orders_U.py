873f89a022764dc705f0f8ddab9232ce4bc369bd
isidora.araya <isidora.araya@ug.uchile.cl>
2022-10-17 13:45:38 +0200

    update on symbolic code

--- codes/polynomial_orders_U.py
+++ codes/polynomial_orders_U.py
@@ -2,7 +2,6 @@
 #
 # See [this hackmd](https://hackmd.io/Rpt2C8oOQ2SGkGS9OYrlfQ?view) for the motivation and the expressions
 
-import pdb
 import numpy as np
 import sympy
 from sympy import (
@@ -84,7 +83,7 @@ sympy.block_collapse(H_tilde_n[1]).blocks[1, 0] # should be 0 and give condition
 
 # ### Computing $U_n$ and $V_n$
 
-def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
+def compute_next_orders(H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_order):
     """
     H_0 : np Hamiltonian in eigenbasis and ordered by eigenenergy.
     H_p : np Hamiltonian in eigenbasis of H_0
@@ -95,43 +94,33 @@ def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
     U_BBn : list of BB block matrices up to order wanted_order
     V_ABn : list of AB block matrices up to order wanted_order
     """
-    N = H_0.shape[0]
-    if N_A is None:
-        N_A = N // 2
-    N_B = N - N_A
+    H_0_AA = np.array(H_0_AA)
+    H_0_BB = np.array(H_0_BB)
+    H_p_AA = np.array(H_p_AA)
+    H_p_BB = np.array(H_p_BB)
+    H_p_AB = np.array(H_p_AB)
     
-    if type(H_p) == type(Matrix()):
-        H_0_AA = np.array( diag(*['E_{}'.format(i) for i in range(1,N_A+1)]) )
-        H_0_BB = np.array( diag(*['E_{}'.format(i) for i in range(N_A+1,N_A+N_B+1)]) )
-        H_p_AA = np.array(H_p[:N_A, :N_A])
-        H_p_AB = np.array(H_p[:N_A, N_A:])
-        H_p_BB = np.array(H_p[N_A:, N_A:])
+    N_A = H_0_AA.shape[0]
+    N_B = H_0_BB.shape[0]
+    
+    assert H_p_AA.shape[0]==N_A
+    assert H_p_BB.shape[0]==N_B
+    assert H_p_AB.shape[0]==N_A
+    assert H_p_AB.shape[1]==N_B
         
-    else:
-        H_0_AA = H_0[:N_A, :N_A]
-        H_0_BB = H_0[N_A:, N_A:]
-        H_p_AA = H_p[:N_A, :N_A]
-        H_p_AB = H_p[:N_A, N_A:]
-        H_p_BB = H_p[N_A:, N_A:]
-    
     # Blocks of U and V
     # 0th order
     U_AAn = [np.eye(N_A, dtype=complex)]
     U_BBn = [np.eye(N_B, dtype=complex)]
     V_ABn = [np.zeros((N_A, N_B), dtype=complex)]
+    
     if wanted_order == 0:
         return U_AAn, U_BBn, V_ABn
         
     #1st order
-    if type(H_p) == type(Matrix()):
-        E_A = np.diag(np.array(H_0_AA)) #np.diag(H_0)[:N_A]
-        E_B = np.diag(np.array(H_0_BB)) #np.diag(H_0)[N_A:]
-        energy_denominators = 1/(E_A.reshape(-1, 1) - E_B)
-    
-    else:
-        E_A = np.diag(H_0)[:N_A]
-        E_B = np.diag(H_0)[N_A:]
-        energy_denominators = 1/(E_A.reshape(-1, 1) - E_B)
+    E_A = np.diag(np.array(H_0_AA))
+    E_B = np.diag(np.array(H_0_BB))
+    energy_denominators = 1/(E_A.reshape(-1, 1) - E_B)
     
     U_AAn.append(np.zeros((N_A, N_A), dtype=complex))
     U_BBn.append(np.zeros((N_B, N_B), dtype=complex))
@@ -148,26 +137,24 @@ def compute_next_orders(H_0, H_p, wanted_order, N_A=None):
             Y_next = Y_next - (
                 + U_AAn[n-i-1] @ H_p_AA @ V_ABn[i]
                 + U_AAn[n-i-1] @ H_p_AB @ U_BBn[i]
-                - V_ABn[n-i-1] @ H_p_AB.conj().T @ V_ABn[i]
+                - V_ABn[n-i-1] @ Dagger(H_p_AB) @ V_ABn[i]
                 - V_ABn[n-i-1] @ H_p_BB @ U_BBn[i]
             )
         for i in range(1, n):
             Y_next = Y_next - U_AAn[n-i] @ H_0_AA @ V_ABn[i] - V_ABn[n-i] @ H_0_BB @ U_BBn[i]
-            U_AA_next = U_AA_next - (U_AAn[n-i] @ U_AAn[i] + V_ABn[n-i] @ V_ABn[i].conj().T) / 2
-            U_BB_next = U_BB_next - (U_BBn[n-i] @ U_BBn[i] + V_ABn[n-i].conj().T @ V_ABn[i]) / 2
+            U_AA_next = U_AA_next - (U_AAn[n-i] @ U_AAn[i] + V_ABn[n-i] @ Dagger(V_ABn[i])) / 2
+            U_BB_next = U_BB_next - (U_BBn[n-i] @ U_BBn[i] + Dagger(V_ABn[n-i]) @ V_ABn[i]) / 2
         
-        if type(H_p) == type(np.ndarray):
+        if isinstance(H_p, np.ndarray):
             if any(not np.all(np.isfinite(mat)) for mat in (U_AA_next, U_BB_next, Y_next)):
                 raise RuntimeError(f"Instability encountered in {n}th order.")
         U_AAn.append(U_AA_next)
         U_BBn.append(U_BB_next)
         V_ABn.append(Y_next * energy_denominators)
-    pdb.set_trace()   
     if type(H_p) == type(Matrix()):
         U_AAn = [Matrix(u) for u in U_AAn]
         U_BBn = [Matrix(u) for u in U_BBn]
         V_ABn = [Matrix(u) for u in V_ABn]
-
         
     return U_AAn, U_BBn, V_ABn
 
@@ -181,28 +168,29 @@ N_B = 2
 N = N_A + N_B
 H_0 = np.diag(np.sort(np.random.randn(N)))
 
-
-
 kx, ky, kz = symbols('k_x k_y k_z')
 a, b, c = symbols('a b c')
 H_p = Matrix([[a*(kx**2+ky**2)/2,0,c*kz,0],[0,b*(kx**2+ky**2)/2,0,c*kz],[0,0,a*(kx**2+ky**2)/2,0],[0,0,0,b*(kx**2+kz**2)/2]])
 H_p += Dagger(H_p).T
 H_p = H_p
 
-pdb.set_trace()
-U_AAn, U_BBn, V_ABn = compute_next_orders(H_0,H_p,wanted_order=wanted_order,N_A=N_A)
+H_0_AA = H_0[:N_A, :N_A]
+H_0_BB = H_0[N_B:, N_B:]
+H_p_AA = H_p[:N_A, :N_A]
+H_p_BB = H_p[N_B:, N_B:]
+H_p_AB = H_p[:N_A, N_B:]
 
-# %time U_AAn, U_BBn, V_ABn = compute_next_orders(H_0, H_p, wanted_order, N_A=N_A)
+U_AAn, U_BBn, V_ABn = compute_next_orders(H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_order=wanted_order)
 
+# %time U_AAn, U_BBn, V_ABn = compute_next_orders(H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_order=wanted_order)
 
-
-
-"""
 # +
 U_n = [np.block([[U_AA, np.zeros((N_A, N_B))], [np.zeros((N_B, N_A)), U_BB]]) for U_AA, U_BB in zip(U_AAn, U_BBn)]
-V_n = [np.block([[np.zeros((N_A, N_A)), V_AB], [-V_AB.conj().T, np.zeros((N_B, N_B))]]) for V_AB in V_ABn]
+V_n = [np.block([[np.zeros((N_A, N_A)), V_AB], [-Dagger(V_AB), np.zeros((N_B, N_B))]]) for V_AB in V_ABn]
 
 H_tilde_n = H_tilde(H_0, H_p, wanted_order, U_n, V_n)
+
+# +
 for H_tilde_ord in H_tilde_n:
     non_hermiticity = np.linalg.norm(H_tilde_ord - H_tilde_ord.T.conj())
     assert non_hermiticity < 1e-10, non_hermiticity
@@ -239,5 +227,7 @@ plt.plot(
 plt.loglog()
 plt.title("Matrices are unitary to given order");
 # -
-"""
 
+H_tilde_n[3]
+
+H_p
