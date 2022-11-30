18130dd04b44ce8fe8dd183c4c00b9782fd20da2
SebMil <s.miles@tudelft.nl>
2022-11-30 14:09:14 +0100

    Replace H_p in unitarity check; more formatting

--- codes/tests/test_polynomial_orders_U.py
+++ codes/tests/test_polynomial_orders_U.py
@@ -89,39 +89,14 @@ def test_check_unitary():
 
     exp_S = polynomial_orders_U.compute_next_orders(H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_orders=wanted_orders)
     
-    #Reinitialize H as identities
-    H_0 = np.eye(N)
-    
-    H_ps = []
-    for perturbation in range(N_p):
-        H_p = np.eye(N)
-        H_ps.append(H_p)
+    #Check unitarity
 
-    H_0_AA = H_0[:N_A, :N_A]
-    H_0_BB = H_0[N_A:, N_A:]
+    H_AA, H_BB, H_AB = polynomial_orders_U.H_tilde(np.eye(N_A), np.eye(N_B), {}, {}, {}, wanted_orders, exp_S, compute_AB=True)
 
-    orders = ta.array(np.eye(N_p))
-    H_p_AA = {
-        order: value[:N_A, :N_A]
-        for order, value in zip(orders, H_ps)
-    }
+    test_AA = np.all([np.allclose(value, 0, atol=precision_tol) for key, value in H_AA.items()])
+    test_BB = np.all([np.allclose(value, 0, atol=precision_tol) for key, value in H_BB.items()])
+    test_AB = np.all([np.allclose(value, 0, atol=precision_tol) for key, value in H_AB.items()])
 
-    H_p_BB = {
-        order: value[N_A:, N_A:]
-        for order, value in zip(orders, H_ps)
-    }
-
-    H_p_AB = {
-        order: value[:N_A, N_A:]
-        for order, value in zip(orders, H_ps)
-    }
-
-    H_AA, H_BB, H_AB = polynomial_orders_U.H_tilde(H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_orders, exp_S, compute_AB=True)
-
-    test_AA = np.all([np.allclose(value, 0, atol=precision_tol) for key ,value in H_AA.items()])
-    test_BB = np.all([np.allclose(value, 0, atol=precision_tol) for key ,value in H_BB.items()])
-    test_AB = np.all([np.allclose(value, 0, atol=precision_tol) for key ,value in H_AB.items()])
-
-    assert test_AA, H_AA.items()
-    assert test_BB, H_BB.items()
-    assert test_AB, H_AB.items()
+    assert test_AA
+    assert test_BB
+    assert test_AB
