88643ef71cc29da26fdeb0d73d113c7d058a3bf1
SebMil <s.miles@tudelft.nl>
2023-01-12 19:04:46 +0100

    Towards SumOfOperatorsProduct(): Intro simplification via loop

--- codes/polynomial_orders_U.py
+++ codes/polynomial_orders_U.py
@@ -21,6 +21,78 @@ import tinyarray as ta
 
 
 # ### Auxiliary functions
+# -
+
+class SumOfOperatorProducts:
+    def __init__(self, terms):
+        self.terms = terms # [[(array, "AB"), (array, "BB")], [(array, "AB")]]
+        self.simplify_products()
+
+    def __add__(self, other):
+        # Actually if AB should add things together;
+        return SumOfOperatorProducts(self.terms + other.terms)
+    
+    def __matmul__(self, other):
+        return SumOfOperatorProducts([a + b for a, b in itertools.product(self.terms, other.terms)])
+    
+    def __truediv__(self, other): # self / other
+        return (1/other) * self
+    
+    def __rmul__(self, other):
+        return
+    
+    def reduce_sublist(self,slist):
+        def elmmul(a,b):
+            return (a[0]@b[0], '{0}{1}'.format(a[1][0],b[1][1]))
+        
+        temp = [slist[0]]
+        for v in slist[1:]:
+            if temp[-1][1][1] == v[1][0] and v[1][0] == 'B':
+                    temp[-1] = elmmul(temp[-1],v)
+            else:
+                temp.append(v)
+        if len(temp)<len(slist):
+            return self.reduce_sublist(temp)
+        elif len(temp)==len(slist):
+            return temp
+    
+    def simplify_products(self):
+        nterms = []
+        for sub_list in self.terms:
+            nterms.append(self.reduce_sublist(sub_list))
+        self.terms = nterms
+        return
+
+
+# +
+from numpy.random import random as rnd
+t_list = [[(rnd((4,10)),'AB'),
+           (rnd((10,10)),'BB'),
+           (rnd((10,4)),'BA'),(rnd((4,4)),'AB')],
+          [(rnd((4,10)),'AB'),
+           (rnd((10,10)),'BB')],
+          [(rnd((4,4)),'AA'),
+           (rnd((4,10)),'AB'),
+           (rnd((10,4)),'BA'),
+           (rnd((4,10)),'AB'),
+           (rnd((10,10)),'BB'),
+           (rnd((10,4)),'BA'),
+           (rnd((4,4)),'AA'),
+           (rnd((4,10)),'AB')]]
+
+t_list_2 = [[(rnd((4,10)),'AB'),
+             (rnd((10,10)),'BB')],
+            [(rnd((10,4)),'BA'),
+             (rnd((4,4)),'AA'),
+             (rnd((4,10)),'AB')]]
+# -
+
+# test term reduction
+test = SumOfOperatorProducts(t_list)
+
+
+test.terms
+
 
 # +
 class Zero:
