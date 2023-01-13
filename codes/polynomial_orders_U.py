25b7447b0d54c05f18d7d5513e724e5d28927e7f
SebMil <s.miles@tudelft.nl>
2023-01-13 12:59:45 +0100

    Add evalf method and c_flag argument

--- codes/polynomial_orders_U.py
+++ codes/polynomial_orders_U.py
@@ -41,13 +41,15 @@ class SumOfOperatorProducts:
     def __rmul__(self, other):
         return
     
-    def reduce_sublist(self,slist):
+    def reduce_sublist(self,slist, c_flag='B'):
+        # This can be made more efficient by getting rid of the surplus loop
+        # to check equality
         def elmmul(a,b):
             return (a[0]@b[0], '{0}{1}'.format(a[1][0],b[1][1]))
         
         temp = [slist[0]]
         for v in slist[1:]:
-            if temp[-1][1][1] == v[1][0] and v[1][0] == 'B':
+            if temp[-1][1][1] == v[1][0] and v[1][0] == c_flag:
                     temp[-1] = elmmul(temp[-1],v)
             else:
                 temp.append(v)
@@ -57,11 +59,14 @@ class SumOfOperatorProducts:
             return temp
     
     def simplify_products(self):
-        nterms = []
-        for sub_list in self.terms:
-            nterms.append(self.reduce_sublist(sub_list))
+        nterms = [self.reduce_sublist(slist) for slist in self.terms]
         self.terms = nterms
         return
+    
+    def evalf(self):
+        temp = [self.reduce_sublist(slist, c_flag='A') for slist in self.terms]
+        return temp
+    
 
 
 # +
