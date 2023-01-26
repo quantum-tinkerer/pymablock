ab2ef69f1f9025f7cd0e04cb28ecd0ff0a8fb397
isidora.araya <isidora.araya@ug.uchile.cl>
2023-01-26 20:21:15 +0100

    not use repeat in test, but fixtures

--- codes/tests/test_polynomial_orders_U.py
+++ codes/tests/test_polynomial_orders_U.py
@@ -7,7 +7,46 @@ import pytest
 from codes.polynomial_orders_U import compute_next_orders, H_tilde
 
 
-def assert_almost_zero(a, decimal, extra_msg=""):
+@pytest.fixture(scope="module", params=range(2, 4))
+def wanted_orders(request):
+    N_p = 4
+    print(request.param)
+    return [
+        np.random.randint(0, high=request.param + 3, size=N_p),
+        ta.array([4] + [0 for i in range(N_p - 1)]),
+    ]
+
+
+@pytest.fixture(scope="module")
+def Ns():
+    return np.random.randint(1, high=5, size=2)
+
+
+@pytest.fixture(scope="module")
+def hamiltonians(Ns, wanted_orders):
+    N_p = len(wanted_orders[0])
+    orders = ta.array(np.eye(N_p))
+    hams = []
+    for i in range(2):
+        hams.append(np.diag(np.sort(np.random.rand(Ns[i]))) + i)
+
+    def matrices_it(N_i, N_j, hermitian):
+        """
+        Generate random matrices of size N_i x N_j.
+        """
+        for i in count():
+            H = np.random.rand(N_i, N_j) + 1j * np.random.rand(N_i, N_j)
+            if hermitian:
+                H += H.conj().T
+            yield H
+
+    for i, j, hermitian in zip([0, 1, 0], [0, 1, 1], [True, True, False]):
+        matrices = matrices_it(Ns[i], Ns[j], hermitian)
+        hams.append({order: matrix for order, matrix in zip(orders, matrices)})
+    return hams
+
+
+def assert_almost_zero(a, decimal=5, extra_msg=""):
     """
     Assert that all values in a are almost zero.
 
@@ -21,95 +60,33 @@ def assert_almost_zero(a, decimal, extra_msg=""):
         )
 
 
-@pytest.fixture(scope="module")
-def decimal():
-    return 5
-
-
-@pytest.fixture(scope="module")
-def N_A():
-    return np.random.randint(1, high=10)
-
-
-@pytest.fixture(scope="module")
-def N_B():
-    return np.random.randint(1, high=10)
-
-
-@pytest.fixture(scope="module")
-@pytest.mark.repeat(10, scope="session")
-def N_p():
-    return np.random.randint(0, high=6)
-
-
-@pytest.fixture(scope="module")
-def wanted_orders(N_p):
-    return [
-        np.random.randint(0, high=3, size=N_p),
-        ta.array([4] + [0 for i in range(N_p - 1)]),
-    ]
-
-
-@pytest.fixture(scope="module")
-def H_0_AA(N_A):
-    return np.diag(np.sort(np.random.rand(N_A)))
-
-
-@pytest.fixture(scope="module")
-def H_0_BB(N_B):
-    return np.diag(np.sort(np.random.rand(N_B)))
-
-
-def matrices_it(N_i, N_j, hermitian):
-    """
-    Generate random matrices of size N_i x N_j.
-    """
-    for i in count():
-        H = np.random.rand(N_i, N_j) + 1j * np.random.rand(N_i, N_j)
-        if hermitian:
-            H += H.conj().T
-        yield H
-
-
-@pytest.fixture(scope="module")
-def H_p_AA(N_A, N_p):
-    orders = ta.array(np.eye(N_p))
-    matrices = matrices_it(N_A, N_A, hermitian=True)
-    return {order: matrix for order, matrix in zip(orders, matrices)}
-
-
-@pytest.fixture(scope="module")
-def H_p_BB(N_B, N_p):
-    orders = ta.array(np.eye(N_p))
-
-    matrices = matrices_it(N_B, N_B, hermitian=True)
-    return {order: matrix for order, matrix in zip(orders, matrices)}
-
-
-@pytest.fixture(scope="module")
-def H_p_AB(N_A, N_B, N_p):
-    orders = ta.array(np.eye(N_p))
-    matrices = matrices_it(N_A, N_B, hermitian=False)
-    return {order: matrix for order, matrix in zip(orders, matrices)}
-
-
-def test_check_AB(decimal, H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_orders):
+def test_check_AB(hamiltonians, wanted_orders):
     """
     Test that H_AB is zero for a random Hamiltonian.
 
-    H_0_AA: N_A x N_A matrix
-    H_0_BB: N_B x N_B matrix
-    H_p_AA: N_p x N_A x N_A matrix
-    H_p_BB: N_p x N_B x N_B matrix
-    H_p_AB: N_p x N_A x N_B matrix
+    hamiltonians:
     wanted_orders: list of orders to compute
     """
-    exp_S = compute_next_orders(
-        H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_orders=wanted_orders
+    exp_S = compute_next_orders(*hamiltonians, wanted_orders=wanted_orders)
+
+    H_AB = H_tilde(*hamiltonians, wanted_orders, exp_S, compute_AB=True)[2]
+
+    assert_almost_zero(H_AB)
+
+
+def test_check_unitary(Ns, hamiltonians, wanted_orders):
+    """
+    Test that the transformation is unitary.
+
+    Ns
+    hamiltonians:
+    wanted_orders: list of orders to compute
+    """
+    decimal = 5
+    exp_S = compute_next_orders(*hamiltonians, wanted_orders=wanted_orders)
+    transformed = H_tilde(
+        np.eye(Ns[0]), np.eye(Ns[1]), {}, {}, {}, wanted_orders, exp_S, compute_AB=True
     )
 
-    H_AB = H_tilde(
-        H_0_AA, H_0_BB, H_p_AA, H_p_BB, H_p_AB, wanted_orders, exp_S, compute_AB=True
-    )[2]
-
-    assert_almost_zero(H_AB, decimal)
+    for value, block in zip(transformed, "AA BB AB".split()):
+        assert_almost_zero(value, decimal, f"{block=}")
