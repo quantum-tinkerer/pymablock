06664465d337078d4ea924d109a78fdc53477264
Isidora Araya <iarayaday@gmail.com>
2023-03-28 16:23:58 +0200

    test U too

--- lowdin/tests/test_block_diagonalization.py
+++ lowdin/tests/test_block_diagonalization.py
@@ -214,19 +214,22 @@ def test_equivalence_general_expanded(H, wanted_orders):
     H: BlockSeries of the Hamiltonian
     wanted_orders: list of orders to compute
     """
-    H_tilde_general, _, _ = general(H)
-    H_tilde_expanded, _, _ = expanded(H)
+    H_tilde_general, U_general, _ = general(H)
+    H_tilde_expanded, U_expanded, _ = expanded(H)
     for order in wanted_orders:
         for block in ((0, 0), (1, 1), (0, 1)):
-            result_general = H_tilde_general.evaluated[block + order]
-            result_expanded = H_tilde_expanded.evaluated[block + order]
-            if zero == result_general:
-                assert zero == result_expanded
-            elif zero == result_expanded:
-                np.testing.assert_allclose(
-                    0, result_general, atol=10**-5, err_msg=f"{order=}"
-                )
-            else:
-                np.testing.assert_allclose(
-                    result_general, result_expanded, atol=10**-5, err_msg=f"{order=}"
-                )
+            for operator_general, operator_expanded in zip(
+                (H_tilde_general, U_general), (H_tilde_expanded, U_expanded)
+            ):
+                result_general = operator_general.evaluated[block + order]
+                result_expanded = operator_expanded.evaluated[block + order]
+                if zero == result_general:
+                    assert zero == result_expanded
+                elif zero == result_expanded:
+                    np.testing.assert_allclose(
+                        0, result_general, atol=10**-5, err_msg=f"{order=}"
+                    )
+                else:
+                    np.testing.assert_allclose(
+                        result_general, result_expanded, atol=10**-5, err_msg=f"{order=}"
+                    )
