165a509c032bbdf54571f269eef382a8074268d7
SebMil <s.miles@tudelft.nl>
2023-04-07 13:20:35 +0200

    Reinsert pol_kpm tests and make them SoP ready

--- lowdin/tests/test_poly_kpm.py
+++ lowdin/tests/test_poly_kpm.py
@@ -5,12 +5,12 @@ import numpy as np
 import tinyarray as ta
 from scipy.linalg import eigh, block_diag
 
-from lowdin.poly_kpm import SumOfOperatorProducts, divide_energies, create_div_energs
-from lowdin.polynomial_orders_U import compute_next_orders
+from lowdin.poly_kpm import SumOfOperatorProducts, create_div_energs
 from lowdin.linalg import ComplementProjector, complement_projected
 
 
-pytest.skip("This test is not yet ready for new api", allow_module_level=True)
+#pytest.skip("This test is not yet ready for new api", allow_module_level=True)
+
 
 @pytest.fixture(
     scope="module",
@@ -103,6 +103,7 @@ def random_term(n, m, length, start, end, rng=None):
     ops = [rng.random(size=dim) for dim in op_dims]
     return SumOfOperatorProducts([[(op, space) for op, space in zip(ops, op_spaces)]])
 
+
 def assert_almost_zero(a, decimal=5, extra_msg=""):
     """
     Assert that all values in a are almost zero.
@@ -116,7 +117,9 @@ def assert_almost_zero(a, decimal=5, extra_msg=""):
             value, 0, decimal=decimal, err_msg=f"{key=} {extra_msg}"
         )
 
+
 # ############################################################################################
+
 
 def test_shape_validation():
     """Test that only terms of compatible shapes are accepted.
@@ -148,6 +151,7 @@ def test_shape_validation():
             with pytest.raises(ValueError):
                 term1 @ term2
 
+
 def test_neg():
     """Test that negation works."""
     n, m = 4, 10
@@ -158,174 +162,69 @@ def test_neg():
     np.testing.assert_allclose(zero.terms[0][0][0], 0)
 
 
-# test if arrays and SumOfOperatorProducts generates the ame terms
-@pytest.mark.xfail
-def test_array_vs_sop(hamiltonians, wanted_orders):
-    n_a, n_b = hamiltonians[0].shape[0], hamiltonians[1].shape[0]
+def test_create_div_energs_kpm(hamiltonians):
+    n_a = hamiltonians[0].shape[0]
+    n_b = hamiltonians[1].shape[0]
+    h_0 = block_diag(hamiltonians[0], hamiltonians[1])
+    eigs, vecs = eigh(h_0)
+    eigs_a, vecs_a = eigs[:n_a], vecs[:, :n_a]
+    eigs_b, vecs_b = eigs[n_a:], vecs[:, n_a:]
 
-    H_0 = np.diag(np.sort(np.random.random(n_a+n_b)))
+    Y = []
+    for _ in range(5):
+        h_ab = np.random.random((n_a + n_b, n_a + n_b)) + 1j * np.random.random(
+            (n_a + n_b, n_a + n_b)
+        )
+        h_ab += h_ab.conj().T
+        Y.append(vecs_a.conj().T @ h_ab @ ComplementProjector(vecs_a))
 
-    # initialize arrays
-    H_0_AA_arr = hamiltonians[0]
-    H_0_BB_arr = hamiltonians[1]
+    de_kpm_func = lambda Y: create_div_energs(
+        h_0, vecs_a, eigs_a, kpm_params=dict(num_moments=1000)
+    )(Y)
+    de_exact_func = lambda Y: create_div_energs(h_0, vecs_a, eigs_a, vecs_b, eigs_b)(Y)
 
-    H_p_AA_arr = hamiltonians[2]
-    H_p_AB_arr = hamiltonians[4]
-    H_p_BB_arr = hamiltonians[3]
+    # apply h_ab from left -> Y.conj() since G_0 is hermitian
+    applied_exact = [de_exact_func(y.conj()) for y in Y]
+    applied_kpm = [de_kpm_func(y.conj()) for y in Y]
 
-    exp_S_arr = compute_next_orders(H_0_AA_arr,
-                                    H_0_BB_arr,
-                                    H_p_AA_arr,
-                                    H_p_BB_arr,
-                                    H_p_AB_arr,
-                                    wanted_orders=wanted_orders
-                                    )
+    diff_approach = {
+        i: np.abs(applied_exact[i] - applied_kpm[i]) for i in range(len(Y))
+    }
 
-    # initialize SumOfOps
-    H_0_AA_sop = SumOfOperatorProducts([[(H_0_AA_arr,'AA')]])
-    H_0_BB_sop = SumOfOperatorProducts([[(H_0_BB_arr,'BB')]])
+    assert_almost_zero(diff_approach, decimal=1, extra_msg="")
 
-    H_p_AA_sop = {key:SumOfOperatorProducts([[(val,'AA')]]) for key,val in H_p_AA_arr.items()}
-    H_p_AB_sop = {key:SumOfOperatorProducts([[(val,'AB')]]) for key,val in H_p_AB_arr.items()}
-    H_p_BB_sop = {key:SumOfOperatorProducts([[(val,'BB')]]) for key,val in H_p_BB_arr.items()}
-
-    exp_S_sop = compute_next_orders(H_0_AA_sop,
-                                    H_0_BB_sop,
-                                    H_p_AA_sop,
-                                    H_p_BB_sop,
-                                    H_p_AB_sop,
-                                    wanted_orders=wanted_orders,
-                                    divide_energies=lambda Y:divide_energies(Y, H_0_AA_sop, H_0_BB_sop, mode='op')
-                                    )
-    
-    # make all SOPs matrices
-    for i in (0,1):
-        for j in (0,1):
-            exp_S_sop[i,j] = {k:v.to_array() for k,v in exp_S_sop[i,j].items() if isinstance(v,SumOfOperatorProducts)}
-            
-    # subttract two results
-    exp_S_diff = np.zeros_like(exp_S_arr)
-    
-    for i in (0,1):
-        for j in (0,1):
-            temp = {}
-            for key,val in exp_S_arr[i,j].items():
-                if isinstance(val,np.ndarray):
-                    if not val.shape == exp_S_sop[i,j][key].shape:
-                        print('hier')
-                    temp[key]=val-exp_S_sop[i,j][key]
-            exp_S_diff[i,j] = temp
-    exp_S_diff = [exp_S_diff[0,0], exp_S_diff[1,1], exp_S_diff[0,1]]      
-    for value, block in zip(exp_S_diff, "AA BB AB".split()):
-        assert_almost_zero(value, 6, extra_msg=f"{block=}")
-
-
-def test_array_vs_proj(hamiltonians, wanted_orders):
-    n_a, n_b = hamiltonians[0].shape[0], hamiltonians[1].shape[0]
-
-    # initialize arrays
-    H_0_AA_arr = hamiltonians[0]
-    H_0_BB_arr = hamiltonians[1]
-    
-    eigs_a = np.diag(H_0_AA_arr)
-    egs, vcs = eigh(block_diag(H_0_AA_arr,H_0_BB_arr))
-    
-    vecs_a = vcs[:,[np.where(np.isclose(e,egs))[0][0] for e in eigs_a]]
-
-    H_p_AA_arr = hamiltonians[2]
-    H_p_AB_arr = hamiltonians[4]
-    H_p_BB_arr = hamiltonians[3]
-
-    exp_S_arr = compute_next_orders(H_0_AA_arr,
-                                    H_0_BB_arr,
-                                    H_p_AA_arr,
-                                    H_p_BB_arr,
-                                    H_p_AB_arr,
-                                    wanted_orders=wanted_orders
-                                    )
-
-    # initialize SOP with bookkeeping such that B is A+B
-    
-    # AB -> A, A+B
-    h_ab = {A[0]:np.hstack((B[0],B[1])) for A,B in zip(product(hamiltonians[2].keys(),
-                                                              hamiltonians[4].keys()),
-                                                      product(hamiltonians[2].values(),
-                                                              hamiltonians[4].values())
-                                                      ) if A[0]==A[1] }
-    
-    # BB -> A+B, A+B
-    h_bb = {A[0]:np.block([[B[0],B[2]],[B[2].conj().T,B[1]]]) for A,B in zip(product(hamiltonians[2].keys(),
-                                                                                      hamiltonians[3].keys(),
-                                                                                      hamiltonians[4].keys()),
-                                                                              product(hamiltonians[2].values(),
-                                                                                      hamiltonians[3].values(),
-                                                                                      hamiltonians[4].values()))
-                                                                                       if (A[0]==A[1] and 
-                                                                                           A[0]==A[2])}
-    
-    
-    H_0_AA_sop = SumOfOperatorProducts([[(hamiltonians[0],'AA')]])
-    H_0_BB_sop = SumOfOperatorProducts([[(complement_projected(block_diag(hamiltonians[0],hamiltonians[1]), np.eye(n_a+n_b)[:,:n_a]), 'BB')]])
-
-    H_p_AA_sop = {key:SumOfOperatorProducts([[(val,'AA')]]) for key,val in hamiltonians[2].items()}
-    H_p_AB_sop = {key:SumOfOperatorProducts([[(val,'AB')]]) for key,val in h_ab.items()}
-    H_p_BB_sop = {key:SumOfOperatorProducts([[(complement_projected(val, np.eye(n_a+n_b)[:,:n_a]),'BB')]]) for key,val in h_bb.items()}
-
-    exp_S_sop = compute_next_orders(H_0_AA_sop,
-                                    H_0_BB_sop,
-                                    H_p_AA_sop,
-                                    H_p_BB_sop,
-                                    H_p_AB_sop,
-                                    wanted_orders=wanted_orders,
-                                    divide_energies=lambda Y:divide_energies(Y, H_0_AA_sop, H_0_BB_sop, mode='op')
-                                    )
-    # make all SOPs matrices
-    for i in (0,1):
-        for j in (0,1):
-            if (i == 0 and j == 0):
-                exp_S_sop[i,j] = {k:v.to_array() for k,v in exp_S_sop[i,j].items() if isinstance(v,SumOfOperatorProducts)}
-            if (i == 0 and j == 1):
-                exp_S_sop[i,j] = {k:v.to_array()[:,n_a:] for k,v in exp_S_sop[i,j].items() if isinstance(v,SumOfOperatorProducts)}
-            if (i == 1 and j == 0):
-                exp_S_sop[i,j] = {k:v.to_array()[n_a:,:] for k,v in exp_S_sop[i,j].items() if isinstance(v,SumOfOperatorProducts)}
-            if (i == 1 and j == 1):
-                exp_S_sop[i,j] = {k:v.to_array()[n_a:,n_a:] for k,v in exp_S_sop[i,j].items() if isinstance(v,SumOfOperatorProducts)}
-            
-    # subttract two results
-    exp_S_diff = np.zeros_like(exp_S_arr)
-    
-    for i in (0,1):
-        for j in (0,1):
-            temp = {}
-            for key,val in exp_S_arr[i,j].items():
-                if isinstance(val,np.ndarray):
-                    temp[key]=val-exp_S_sop[i,j][key]
-            exp_S_diff[i,j] = temp
-    exp_S_diff = [exp_S_diff[0,0], exp_S_diff[1,1], exp_S_diff[0,1]]      
-    for value, block in zip(exp_S_diff, "AA BB AB".split()):
-        assert_almost_zero(value, 6, extra_msg=f"{block=}")
 
 def test_create_div_energs_kpm(hamiltonians):
     n_a = hamiltonians[0].shape[0]
     n_b = hamiltonians[1].shape[0]
-    h_0 = block_diag(hamiltonians[0],hamiltonians[1])
+    h_0 = block_diag(hamiltonians[0], hamiltonians[1])
     eigs, vecs = eigh(h_0)
-    eigs_a, vecs_a = eigs[:n_a], vecs[:,:n_a]
-    eigs_b, vecs_b = eigs[n_a:], vecs[:,n_a:]
-    
+    eigs_a, vecs_a = eigs[:n_a], vecs[:, :n_a]
+    eigs_b, vecs_b = eigs[n_a:], vecs[:, n_a:]
+
     Y = []
     for _ in range(5):
-        h_ab = np.random.random((n_a+n_b,n_a+n_b)) + 1j * np.random.random((n_a+n_b, n_a+n_b))
+        h_ab = np.random.random((n_a + n_b, n_a + n_b)) + 1j * np.random.random(
+            (n_a + n_b, n_a + n_b)
+        )
         h_ab += h_ab.conj().T
-        Y.append(vecs_a.conj().T @ h_ab @ ComplementProjector(vecs_a))
-    
-    de_kpm_func =lambda Y: create_div_energs(h_0, vecs_a, eigs_a, kpm_params = dict(num_moments=1000))(Y)
-    de_exact_func =lambda Y: create_div_energs(h_0, vecs_a, eigs_a, vecs_b, eigs_b)(Y)
-    
-    #apply h_ab from left -> Y.conj() since G_0 is hermitian
-    applied_exact = [de_exact_func(y.conj()) for y in Y]
-    applied_kpm = [de_kpm_func(y.conj()) for y in Y]
-    
-    diff_approach = {i:np.abs(applied_exact[i] - applied_kpm[i]) for i in range(len(Y))}
-    
+        Y.append(
+            SumOfOperatorProducts(
+                [[(vecs_a.conj().T @ h_ab @ ComplementProjector(vecs_a), "AB")]]
+            )
+        )
+
+    de_kpm_func = lambda Y: create_div_energs(
+        h_0, vecs_a, eigs_a, kpm_params=dict(num_moments=1000)
+    )(Y)
+    de_exact_func = lambda Y: create_div_energs(h_0, vecs_a, eigs_a, vecs_b, eigs_b)(Y)
+
+    # apply h_ab from left -> Y.conj() since G_0 is hermitian
+    applied_exact = [de_exact_func(y.conjugate()) for y in Y]
+    applied_kpm = [de_kpm_func(y.conjugate()) for y in Y]
+
+    diff_approach = {
+        i: np.abs(applied_exact[i].to_array() - applied_kpm[i].to_array()) for i in range(len(Y))
+    }
+
     assert_almost_zero(diff_approach, decimal=1, extra_msg="")
