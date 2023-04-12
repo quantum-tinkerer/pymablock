03353bafef26c620361a2146360f082ed6c38aa2
Isidora Araya <iarayaday@gmail.com>
2023-04-12 18:21:44 +0200

    use another template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,7 @@ release = '.'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # 'myst_parser',
-    'sphinx_mdinclude',
+    'myst_parser',
 ]
 
 
@@ -52,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
