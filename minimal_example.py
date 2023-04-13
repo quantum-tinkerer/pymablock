8c60471645e3ea172070f4f3b68a9febb7b1866d
Sebastian Miles <s.miles@tudelft.nl>
2023-04-13 12:54:16 +0200

    minimal error example

---
+++ minimal_example.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy
+import sympy
+import tinyarray as ta
+from lowdin import linalg, block_diagonalization, poly_kpm
+from lowdin.tests import test_block_diagonalization
+from lowdin.linalg import complement_projected, ComplementProjector
+
+
