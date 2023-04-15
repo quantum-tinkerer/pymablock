c226f24a2ecebcc898545cc1654fad624e014044
Anton Akhmerov <anton.akhmerov@gmail.com>
2023-04-15 18:06:48 +0200

    add numerical to public functions

--- lowdin/__init__.py
+++ lowdin/__init__.py
@@ -1,6 +1,19 @@
 "Lowdin perturbation theory"
 
-from .block_diagonalization import general, expanded, general_symbolic, to_BlockSeries
+from .block_diagonalization import (
+    general,
+    expanded,
+    general_symbolic,
+    numerical,
+    to_BlockSeries,
+)
 from . import series
 
-__all__ = ["general", "expanded", "general_symbolic", "to_BlockSeries", "series"]
+__all__ = [
+    "general",
+    "expanded",
+    "general_symbolic",
+    "numerical",
+    "to_BlockSeries",
+    "series",
+]
