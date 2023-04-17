6ec9c383f09d5fa14605cb86de4815414498944f
Sebastian Miles <s.miles@tudelft.nl>
2023-04-17 15:58:34 +0200

    Now really with the repsective files

--- minimal_example.py
+++ minimal_example.py
@@ -21,43 +21,6 @@ eigs, vecs = scipy.linalg.eigh(h_0)
 eigs_a = eigs[:2]
 vecs_a = vecs[:,:2]
 
-eigs_b = eigs[2:]
-vecs_b = vecs[:,2:]
-
-h = {ta.array([0 for _ in range(n)]): h_0}
-
-for i in range(n):
-    h_p = np.random.random((dim, dim)) + 1j * np.random.random((dim, dim))
-    h_p += h_p.conj().transpose()
-    index = np.zeros(n,int)
-    index[i] = 1
-    h[ta.array(index)] = h_p
-# -
-
-h_tilde, u, u_adj = block_diagonalization.numerical(h, vecs_a, eigs_a, kpm_params={'num_moments':1000})
-
-h_tilde.evaluated[0,0,4]
-
-h_tilde.evaluated[1,1,3] 
-
-
-
-
-
-# +
-n = 1
-dim = 25
-
-h_0 = np.random.random((dim, dim)) + 1j * np.random.random((dim, dim))
-h_0 += h_0.conj().transpose()
-
-#h_0 = np.diag(np.sort(np.random.random(dim)))
-
-eigs, vecs = scipy.linalg.eigh(h_0)
-
-eigs_a = eigs[:2]
-vecs_a = vecs[:,:2]
-
 eigs_b = eigs[2:10]
 vecs_b = vecs[:,2:10]
 
@@ -148,20 +111,3 @@ def test_check_AB_KPM(wanted_orders: list[tuple[int, ...]]) -> None:
 
 
 test_check_AB_KPM([(3,)])
-
-np.floor(3/2)
-
-# +
-test = np.random.random((25,25)) + 1j * np.random.random((25,25))
-test += test.conjugate().transpose()
-
-eigs, vecs = scipy.linalg.eigh(test)
-# -
-
-np.allclose(eigs, np.diag(vecs.conjugate().transpose() @ test @ vecs))
-
-np.allclose(test, vecs @ np.diag(eigs) @ vecs.conjugate().transpose())
-
-
-
-
