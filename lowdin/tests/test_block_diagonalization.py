8ed054272bdc6755e996c5a86dd6326e2212a7fc
Isidora Araya <iarayaday@gmail.com>
2023-04-26 02:56:38 +0200

    mark test xfail

--- lowdin/tests/test_block_diagonalization.py
+++ lowdin/tests/test_block_diagonalization.py
@@ -656,7 +656,7 @@ def test_solve_sylvester(
     )
 
 
-@pytest.mark.skip(reason="Sometimes it fails due to precision.")
+@pytest.mark.xfail(reason="Sometimes it fails due to precision.")
 def test_check_AA_numerical(
     generate_kpm_hamiltonian: tuple[
         BlockSeries, np.ndarray, np.ndarray, np.ndarray, np.ndarray
