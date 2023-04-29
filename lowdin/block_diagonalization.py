95264ea322482a64c5a4f3021af8c971dca4ffa1
Isidora Araya <iarayaday@gmail.com>
2023-04-30 01:43:45 +0200

    raise error

--- lowdin/block_diagonalization.py
+++ lowdin/block_diagonalization.py
@@ -809,6 +809,8 @@ def _symbolic_keys_to_orders(hamiltonian):
 
     new_hamiltonian = {}
     for key, value in hamiltonian.items():
+        if isinstance(value, sympy.MatrixBase):
+            raise ValueError("Values of the dictionary must be numeric.")
         if value.dtype == np.dtype("O"):
             raise ValueError("Values of the dictionary must be numeric.")
         monomial = key.as_powers_dict()
