a46d6ff755a6946bfb71759841407b6b1d647036
Anton Akhmerov <anton.akhmerov@gmail.com>
2023-05-31 00:17:56 +0200

    implement using version from git

--- pymablock/__init__.py
+++ pymablock/__init__.py
@@ -1,5 +1,11 @@
 "Quasi-degenerate perturbation theory"
 
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    __version__ = "unknown"
+    __version_tuple__ = (0, 0, "unknown", "unknown")
+
 from .block_diagonalization import (
     block_diagonalize,
     general,
@@ -16,4 +22,6 @@ __all__ = [
     "symbolic",
     "implicit",
     "series",
+    "__version__",
+    "__version_tuple__",
 ]
