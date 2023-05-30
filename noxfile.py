501905f9c6b820604d32a911fc97575676d2aea5
Isidora Araya <iarayaday@gmail.com>
2023-05-30 18:23:41 +0200

    clean noxfile versions

--- noxfile.py
+++ noxfile.py
@@ -6,20 +6,11 @@ import nox
     "python,numpy,scipy,sympy",
     [
         (python, numpy, scipy, sympy)
-        for python in ("3.9", "3.10", "3.11")
-        for numpy in ("1.23.0", "1.24.0")
-        for scipy in ("1.8.0", "1.10.0")
-        for sympy in ("1.10.0", "1.12.0")
-        if (
-            (python, numpy) != ("3.9", "1.24.0")
-            and (python, numpy) != ("3.10", "1.23.0")
-            and (python, numpy) != ("3.11", "1.23.0")
-            and (python, scipy) != ("3.9", "1.10.0")
-            and (python, scipy) != ("3.10", "1.8.0")
-            and (python, scipy) != ("3.11", "1.8.0")
-            and (python, sympy) != ("3.9", "1.12.0")
-            and (python, sympy) != ("3.10", "1.12.0")
-            and (python, sympy) != ("3.11", "1.10.0")
+        for python, numpy, scipy, sympy in zip(
+            ("3.9", "3.10", "3.11"),
+            ("1.23.0", "1.24.0", "1.24.0"),
+            ("1.8.0", "1.10.0", "1.10.0"),
+            ("1.10.0", "1.10.0", "1.12.0"),
         )
     ],
 )
