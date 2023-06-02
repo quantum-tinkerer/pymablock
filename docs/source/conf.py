cf7ecc23147c6cae1efc2445f33b84c3cc8fae27
Isidora Araya <iarayaday@gmail.com>
2023-06-02 16:47:11 +0200

    remove css unused file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,4 +103,3 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_build/html/_static", "_static"]
-html_css_files = ["css/custom.css"]
