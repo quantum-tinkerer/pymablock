8c7e31622f0c6908489b4fb7bd40cffc62e1f6b9
Anton Akhmerov <anton.akhmerov@gmail.com>
2023-06-05 13:27:03 +0200

    add sphinx-tippy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     "myst_nb",
     "sphinx_togglebutton",
     "sphinx_copybutton",
+    "sphinx_tippy",
 ]
 myst_enable_extensions = [
     "dollarmath",
@@ -106,3 +107,4 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["local.css"]
