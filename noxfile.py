ce7f7dad0f28f0648773b415e93ec58d9b9757d1
Anton Akhmerov <anton.akhmerov@gmail.com>
2024-01-31 00:51:27 +0100

    make latest tests latest again

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ import nox
     [
         ("3.9", "1.23.0", "1.8.0", "1.10.0"),
         ("3.10", "1.24", "1.10", "1.10"),
-        ("3.11", "1.24", "1.10", "1.12"),
+        ("3.11", "1.26", "1.12", "1.12"),
     ],
     ids=["minimal", "mid", "latest"],
 )
@@ -16,9 +16,9 @@ def tests(session, numpy, scipy, sympy):
         "mamba",
         "install",
         "-y",
-        f"numpy=={numpy}",
-        f"scipy=={scipy}",
-        f"sympy=={sympy}",
+        f"numpy={numpy}",
+        f"scipy={scipy}",
+        f"sympy={sympy}",
         "python-mumps>=0.0.1,<0.1",
         "packaging==22.0",
         "pytest-cov",
