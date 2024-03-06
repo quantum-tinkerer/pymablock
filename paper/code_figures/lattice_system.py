debc61cc37b489274746c4a8df6f184f34731bba
Isidora Araya <iarayaday@gmail.com>
2024-03-06 19:29:43 +0100

    merge figure

--- paper/code_figures/lattice_system.py
+++ paper/code_figures/lattice_system.py
@@ -47,76 +47,20 @@ syst[(hop for hop in syst.hoppings() if barrier(*hop))] = (
 )
 sysf = syst.finalized()
 # %%
-fig, ax = plt.subplots()
-kwant.plot(
-    syst,
-    fig_size=(10, 6),
-    site_color=(lambda site: abs(site.pos[0]) < L / 3),
-    colorbar=False,
-    cmap="seismic",
-    hop_lw=0,
-    ax=ax,
-)
-ax.set_aspect("equal")
-ax.set_frame_on(False)
-ax.set_xticks([])
-ax.set_yticks([])
-
-ax.text(
-    0.5,
-    0.5,
-    r"$\mu_{\textrm{sc}}, \Delta_{\textrm{sc}}$",
-    ha="center",
-    va="center",
-    transform=ax.transAxes,
-    backgroundcolor="white",
-)
-ax.text(
-    0.2,
-    0.5,
-    r"$\mu_{\textrm{N}}, \Delta_{\textrm{N}}$",
-    ha="center",
-    va="center",
-    transform=ax.transAxes,
-    backgroundcolor="white",
-)
-ax.text(
-    0.8,
-    0.5,
-    r"$\mu_{\textrm{N}}, \Delta_{\textrm{N}}$",
-    ha="center",
-    va="center",
-    transform=ax.transAxes,
-    backgroundcolor="white",
-)
-
-ax.text(0.5, -0.1, rf"$L={L}$", ha="center", va="center", transform=ax.transAxes)
-ax.text(
-    0.02,
-    0.5,
-    rf"$W={W}$",
-    ha="center",
-    va="center",
-    transform=ax.transAxes,
-    rotation=90,
-)
-
-fig.savefig("../figures/QD_lattice.pdf", bbox_inches="tight")
-# %%
 params = dict(mu_n=0.05, mu_sc=0.3, Delta=0.05, t=1.0, t_barrier=0.0)
 h_0 = sysf.hamiltonian_submatrix(params=params, sparse=True).real
 
 barrier = sysf.hamiltonian_submatrix(
     params={**{p: 0 for p in params.keys()}, "t_barrier": 1}, sparse=True
 ).real
-delta_mu = (
+dmu = (
     kwant.operator.Density(sysf, (lambda site: sigma_z * site.pos[0] / L)).tocoo().real
 )
 # %%
 vals, vecs = eigsh(h_0, k=4, sigma=0)
 vecs, _ = scipy.linalg.qr(vecs, mode="economic")  # orthogonalize the vectors
 
-H_tilde, *_ = block_diagonalize([h_0, barrier, delta_mu], subspace_eigenvectors=[vecs])
+H_tilde, *_ = block_diagonalize([h_0, barrier, dmu], subspace_eigenvectors=[vecs])
 # %%
 # Combine all the perturbative terms into a single 4D array
 fill_value = np.zeros((), dtype=object)
@@ -124,26 +68,26 @@ fill_value[()] = np.zeros_like(H_tilde[0, 0, 0, 0])
 h_tilde = np.array(np.ma.filled(H_tilde[0, 0, :3, :3], fill_value).tolist())
 
 
-def effective_energies(h_tilde, barrier, delta_mu):
+def effective_energies(h_tilde, barrier, dmu):
     barrier_powers = barrier ** np.arange(3).reshape(-1, 1, 1, 1)
-    delta_mu_powers = delta_mu ** np.arange(3).reshape(1, -1, 1, 1)
+    dmu_powers = dmu ** np.arange(3).reshape(1, -1, 1, 1)
     return scipy.linalg.eigvalsh(
-        np.sum(h_tilde * barrier_powers * delta_mu_powers, axis=(0, 1))
+        np.sum(h_tilde * barrier_powers * dmu_powers, axis=(0, 1))
     )
 
 
 barrier_vals = np.array([0, 0.5, 0.75])
-delta_mu_vals = np.linspace(0, 10e-4, num=101)
+dmu_vals = np.linspace(0, 10e-4, num=101)
 results = [
-    np.array([effective_energies(h_tilde, bar, dmu) for dmu in delta_mu_vals])
+    np.array([effective_energies(h_tilde, bar, dmu) for dmu in dmu_vals])
     for bar in barrier_vals
 ]
 # %%
-fig, ax = plt.subplots(figsize=(figwidth, figwidth / 3))
+fig, ax = plt.subplots(figsize=(figwidth, figwidth / 4))
 
 [
     ax.plot(
-        delta_mu_vals,
+        dmu_vals,
         result,
         color=color,
         label=[f"$t_b={barrier}$"] + 3 * [None],
@@ -153,7 +97,83 @@ fig, ax = plt.subplots(figsize=(figwidth, figwidth / 3))
 ]
 ax.set_xlabel(r"$\delta_\mu$")
 ax.set_ylabel(r"$E$")
-ax.legend(frameon=False, bbox_to_anchor=(0, -0.2), loc="upper left", ncol=3)
+# ax.legend(frameon=False, bbox_to_anchor=(0, -0.2), loc="upper left", ncol=3)
 ax.spines[["top", "right"]].set_visible(False)
-fig.savefig("../figures/QD_spectrum.pdf", bbox_inches="tight")
+ax.set_ylim(-0.0013, 0.0015)
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+
+ax_inset = fig.add_axes([0.16, 0.76, 0.32, 0.3])
+kwant.plot(
+    syst,
+    fig_size=(10, 6),
+    site_color=(lambda site: abs(site.pos[0]) < L / 3),
+    colorbar=False,
+    cmap="seismic",
+    hop_lw=0,
+    ax=ax_inset,
+)
+ax_inset.set_aspect("equal")
+ax_inset.set_frame_on(False)
+ax_inset.set_xticks([])
+ax_inset.set_yticks([])
+
+ax_inset.text(
+    0.5,
+    0.5,
+    r"$\mu_{\textrm{sc}}, \Delta_{\textrm{sc}}$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="white",
+    fontsize=7,
+    alpha=0.8,
+)
+ax_inset.text(
+    0.2,
+    0.5,
+    r"$\mu_{\textrm{N}}$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="white",
+    fontsize=7,
+    alpha=0.8,
+)
+ax_inset.text(
+    0.8,
+    0.5,
+    r"$\mu_{\textrm{N}}$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="white",
+    fontsize=7,
+    alpha=0.8,
+)
+
+ax_inset.text(
+    0.5,
+    -0.1,
+    rf"$L={L}$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    fontsize=7,
+)
+ax_inset.text(
+    0.02,
+    0.5,
+    rf"$W={W}$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    rotation=90,
+    fontsize=7,
+)
+
+fig.savefig("../figures/lattice_system.pdf", bbox_inches="tight", dpi=300)
+
 # %%
