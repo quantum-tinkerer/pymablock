ff79499a3a9dd251ec52ba7118544ad93ebb2f63
Isidora Araya <iarayaday@gmail.com>
2024-03-20 17:31:31 +0100

    joint editing

--- paper/code_figures/lattice_system.py
+++ paper/code_figures/lattice_system.py
@@ -89,31 +89,168 @@ results = [
 mosaic = [["A", "B"]]
 
 fig, ax = plt.subplot_mosaic(
-    mosaic, figsize=(figwidth, figwidth / 2.2), width_ratios=[1, 1]
+    mosaic, figsize=(figwidth, figwidth / 2.3), width_ratios=[1, 1]
 )
 
 # PANEL A
 h_0_plot, barrier_plot, dmu_plot, _ = system(8, 2, params)
 
-ax["A"].spy(h_0_plot, markersize=0.5, aspect="equal", c=color_cycle[0], label=r"$H_0$")
+ax["A"].spy(
+    h_0_plot, markersize=0.5, aspect="equal", c=color_cycle[0], label=r"$H_{00}$"
+)
 ax["A"].spy(
     barrier_plot,
     markersize=0.5,
     aspect="equal",
     c=color_cycle[2],
-    label=r"$\propto t_b$",
+    label=r"$H_{10}$",
 )
 ax["A"].spy(
     dmu_plot,
     markersize=0.5,
     aspect="equal",
     c=color_cycle[1],
-    label=r"$\propto \delta \mu$",
+    label=r"$H_{01}$",
 )
-ax["A"].set_title(r"$H_0 + H_{1} + H_{2}$")
+ax["A"].set_title(r"$H_{00} + H_{10} + H_{01}$")
 ax["A"].set_xticks([])
 ax["A"].set_yticks([])
 ax["A"].legend(frameon=False, loc="lower left", markerscale=3)
+
+# PANEL A - INSET
+ax_inset = fig.add_axes([0.26, 0.68, 0.2, 0.2])
+rQD1 = matplotlib.patches.Rectangle(
+    (0, 0), L / 3, W, edgecolor="black", facecolor=color_cycle[1], lw=0.5, alpha=0.5
+)
+rSC = matplotlib.patches.Rectangle(
+    (L / 3, 0), L / 3, W, edgecolor="black", facecolor=color_cycle[0], lw=0.5, alpha=0.5
+)
+rQD2 = matplotlib.patches.Rectangle(
+    (2 * L / 3, 0),
+    L / 3,
+    W,
+    edgecolor="black",
+    facecolor=color_cycle[1],
+    lw=0.5,
+    alpha=0.5,
+)
+
+ax_inset.add_patch(rQD1)
+ax_inset.add_patch(rSC)
+ax_inset.add_patch(rQD2)
+
+for barrier_pos in [L / 3, 2 * L / 3]:
+    barrier_line = matplotlib.patches.ConnectionPatch(
+        xyA=(barrier_pos, 0),
+        xyB=(barrier_pos, W),
+        coordsA="data",
+        coordsB="data",
+        axesA=ax_inset,
+        axesB=ax_inset,
+        color=color_cycle[2],
+        lw=1,
+        capstyle="butt",
+    )
+    ax_inset.add_artist(barrier_line)
+
+    # Draw curved arrow across the barrier
+    arrowstyle = matplotlib.patches.ArrowStyle.CurveAB(
+        head_length=0.6,
+        head_width=0.5,
+        widthA=0.01,
+        widthB=0.01,
+        lengthA=0.2,
+        lengthB=0.2,
+        angleA=0,
+        angleB=0,
+        scaleA=None,
+        scaleB=None,
+    )
+    barrier_arrow = matplotlib.patches.FancyArrowPatch(
+        (barrier_pos - L / 17, 2 * W / 3),
+        (barrier_pos + L / 17, 2 * W / 3),
+        connectionstyle="arc3,rad=-0.5",
+        arrowstyle=arrowstyle,
+        color=color_cycle[2],
+    )
+    ax_inset.add_patch(barrier_arrow)
+
+rectangle = matplotlib.patches.Rectangle(
+    (0, 0), L, W, edgecolor="black", facecolor="None", lw=0.5
+)
+
+ax_inset.add_patch(rectangle)
+
+ax_inset.text(
+    0.5,
+    0.5,
+    r"$\mu_{\textrm{sc}}, \Delta$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="None",
+    fontsize=7,
+    alpha=0.8,
+)
+ax_inset.text(
+    0.16,
+    0.5,
+    r"$\mu_{\textrm{N}}, \delta \mu$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="None",
+    fontsize=7,
+    alpha=0.8,
+)
+ax_inset.text(
+    0.84,
+    0.5,
+    r"$\mu_{\textrm{N}}, \delta \mu$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="None",
+    fontsize=7,
+    alpha=0.8,
+)
+ax_inset.text(
+    2 / 3,
+    1.25,
+    r"$t_b$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    backgroundcolor="None",
+    fontsize=7,
+    alpha=0.8,
+)
+ax_inset.text(
+    0.5,
+    -0.3,
+    rf"$L$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    fontsize=7,
+)
+ax_inset.text(
+    -0.04,
+    0.5,
+    rf"$W$",
+    ha="center",
+    va="center",
+    transform=ax_inset.transAxes,
+    rotation=90,
+    fontsize=7,
+)
+
+ax_inset.set_yticks([])
+ax_inset.set_xticks([])
+ax_inset.set_frame_on(False)
+ax_inset.set_xlim(0, L)
+ax_inset.set_ylim(0, W)
+ax_inset.set_aspect("equal")
 
 # PANEL B
 alpha_values = [0.2, 0.5, 1]
@@ -128,85 +265,25 @@ alpha_values = [0.2, 0.5, 1]
     )
     for result, barrier, alpha_val in zip(results, barrier_vals, alpha_values)
 ]
-ax["B"].set_xlabel(r"$\delta_\mu$")
+ax["B"].set_xlabel(r"$\delta \mu$")
 ax["B"].set_ylabel(r"$E$")
 # ax["B"].legend(frameon=False, bbox_to_anchor=(0, -0.2), loc="upper left", ncol=3)
 ax["B"].spines[["top", "right"]].set_visible(False)
 ax["B"].set_ylim(-0.0013, 0.0015)
-ax["B"].set_xticks([])
-ax["B"].set_yticks([])
-ax["B"].set_xticklabels([])
-ax["B"].set_yticklabels([])
+ax["B"].set_xticks([0, 10e-4])
+ax["B"].set_yticks([-0.0005, 0, 0.0005])
+ax["B"].set_xticklabels([r"$0$", r"$10^{-4}$"])
+ax["B"].set_yticklabels([r"$-5$", r"$0$", r"$5$"])
+ax["B"].text(
+    0,
+    1.05,
+    r"$\times 10^{-3}$",
+    ha="center",
+    va="center",
+    transform=ax["B"].transAxes,
+    fontsize=7,
+)
 ax["B"].set_aspect(0.4)
-
-# ax_inset = fig.add_axes([0.25, 0.53, 0.22, 0.2])
-# kwant.plot(
-#     syst,
-#     fig_size=(10, 6),
-#     site_color=(lambda site: abs(site.pos[0]) < L / 3),
-#     colorbar=False,
-#     cmap="seismic",
-#     hop_lw=0,
-#     ax=ax_inset,
-# )
-# ax_inset.set_aspect("equal")
-# ax_inset.set_frame_on(False)
-# ax_inset.set_xticks([])
-# ax_inset.set_yticks([])
-
-# ax_inset.text(
-#     0.5,
-#     0.5,
-#     r"$\mu_{\textrm{sc}}, \Delta$",
-#     ha="center",
-#     va="center",
-#     transform=ax_inset.transAxes,
-#     backgroundcolor="white",
-#     fontsize=7,
-#     alpha=0.8,
-# )
-# ax_inset.text(
-#     0.2,
-#     0.5,
-#     r"$\mu_{\textrm{N}}$",
-#     ha="center",
-#     va="center",
-#     transform=ax_inset.transAxes,
-#     backgroundcolor="white",
-#     fontsize=7,
-#     alpha=0.8,
-# )
-# ax_inset.text(
-#     0.8,
-#     0.5,
-#     r"$\mu_{\textrm{N}}$",
-#     ha="center",
-#     va="center",
-#     transform=ax_inset.transAxes,
-#     backgroundcolor="white",
-#     fontsize=7,
-#     alpha=0.8,
-# )
-
-# ax_inset.text(
-#     0.5,
-#     -0.1,
-#     rf"$L={L}$",
-#     ha="center",
-#     va="center",
-#     transform=ax_inset.transAxes,
-#     fontsize=7,
-# )
-# ax_inset.text(
-#     0.02,
-#     0.5,
-#     rf"$W={W}$",
-#     ha="center",
-#     va="center",
-#     transform=ax_inset.transAxes,
-#     rotation=90,
-#     fontsize=7,
-# )
 
 fig.savefig("../figures/lattice_system.pdf", bbox_inches="tight", dpi=300)
 # %%
