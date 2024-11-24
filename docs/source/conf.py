d7100d30def4bf5d3863c47c3970d3dd193aeed6
Anton Akhmerov <anton.akhmerov@gmail.com>
2024-11-24 16:15:07 +0100

    stop resolving zenodo DOIs in sphinx-tippy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,6 +108,7 @@ html_theme_options = {
     ),
 }
 
+tippy_skip_urls = ["https://doi.org/*zenodo*"]  # Provided by datacite and not crossref.
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
