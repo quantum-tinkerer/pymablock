e9e13629edd41670c5f142077a80d22324bbe490
Anton Akhmerov <anton.akhmerov@gmail.com>
2025-01-08 16:54:10 +0100

    finalize the sympy patch

--- pymablock/operators.py
+++ pymablock/operators.py
@@ -4,26 +4,31 @@ from collections import defaultdict
 from typing import Callable
 
 import sympy
+from packaging.version import parse
 from sympy.physics.quantum import Dagger, boson
 from sympy.physics.quantum.operatorordering import normal_ordered_form
 
 from pymablock.series import zero
 
-
-def _patch_sympy_matrix_adjoint():
-    """Patch sympy's Matrix.adjoint to work with Dagger.
-
-    TODO: Remove this patch once https://github.com/sympy/sympy/pull/27422 is merged
-    and we depend on a version of sympy that includes it.
-    """
+# Monkey patch sympy to propagate adjoint to matrix elements.
+if parse(sympy.__version__) < parse("1.14.0"):
 
     def _eval_adjoint(self):
         return self.transpose().applyfunc(lambda x: x.adjoint())
 
+    def _eval_transpose(self):
+        from sympy.functions.elementary.complexes import conjugate
+
+        if self.is_commutative:
+            return self
+        if self.is_hermitian:
+            return conjugate(self)
+        if self.is_antihermitian:
+            return -conjugate(self)
+        return None
+
     sympy.MatrixBase.adjoint = _eval_adjoint
-
-
-_patch_sympy_matrix_adjoint()
+    sympy.Expr._eval_transpose = _eval_transpose
 
 
 def find_boson_operators(expr: sympy.Expr):
